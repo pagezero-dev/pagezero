0e6977afa85448983c557e54622d828061f84932
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2025-02-03 20:36:13 +1100

    Improve homepage look (#16)

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -2,8 +2,5 @@ import { test, expect } from "@playwright/test"
 
 test("has content", async ({ page }) => {
   await page.goto("http://localhost:3000")
-  await expect(
-    page.getByRole("heading", { name: "Hello world!" }),
-  ).toBeVisible()
   await expect(page.getByText("Database says hello!")).toBeVisible()
 })
