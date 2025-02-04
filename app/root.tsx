b11e67279c36f588c5053fcf4bba11340d502330
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2025-02-04 18:54:14 +1100

    Fix hot reloading for TailwindCSS

--- app/root.tsx
+++ app/root.tsx
@@ -1,35 +1,38 @@
 import { Links, Meta, Outlet, Scripts, useRouteError } from "react-router"
-import type { HeadersFunction } from "react-router"
 
 import styles from "./styles/index.css?url"
 import fonts from "./fonts/inter-normal-latin.woff2?url"
 import { ErrorPage } from "@/ui/error-page"
 
-export const headers: HeadersFunction = () => {
-  return {
-    Link: `<${styles}>; rel=preload; as=style`,
-  }
-}
-
-export default function App() {
+export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <Meta />
-        <link rel="preload" as="style" href={styles}></link>
-        <link rel="preload" as="font" href={fonts}></link>
+        {!import.meta.env.DEV && (
+          // When link preload is present for styles, hot reloading for Tailwind
+          // stops working. As such, let's not render preload links in development.
+          <>
+            <link rel="preload" as="style" href={styles}></link>
+            <link rel="preload" as="font" href={fonts}></link>
+          </>
+        )}
         <link rel="icon" href="data:image/x-icon;base64,AA" />
         <link rel="stylesheet" href={styles}></link>
         <Links />
       </head>
       <body>
-        <Outlet />
+        {children}
         <Scripts />
       </body>
     </html>
   )
+}
+
+export default function App() {
+  return <Outlet />
 }
 
 export function ErrorBoundary() {
