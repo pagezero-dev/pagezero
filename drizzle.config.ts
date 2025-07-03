0fde1910ce753d0725f382d3dd8c53ae64960dad
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2025-07-03 17:06:50 +1000

    Unify drizzle orm config (#90)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,27 @@
 import { defineConfig } from "drizzle-kit"
 import { getLocalSqliteDbUrl } from "./db/utils"
 
+const hasCloudflareCredentials =
+  process.env.CLOUDFLARE_ACCOUNT_ID &&
+  process.env.CLOUDFLARE_DATABASE_ID &&
+  process.env.CLOUDFLARE_API_TOKEN
+
 export default defineConfig({
   dialect: "sqlite",
   schema: "./db/schema.ts",
   out: "./db/migrations",
-  dbCredentials: {
-    url: getLocalSqliteDbUrl(),
-  },
+  ...(hasCloudflareCredentials
+    ? {
+        driver: "d1-http",
+        dbCredentials: {
+          accountId: process.env.CLOUDFLARE_ACCOUNT_ID,
+          databaseId: process.env.CLOUDFLARE_DATABASE_ID,
+          token: process.env.CLOUDFLARE_API_TOKEN,
+        },
+      }
+    : {
+        dbCredentials: {
+          url: getLocalSqliteDbUrl(),
+        },
+      }),
 })
