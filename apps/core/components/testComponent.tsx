3aed8f14a976fdbb127e456b46f973aee8215e1d
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2025-07-17 18:27:54 +1000

    Remove not needed component

--- apps/core/components/testComponent.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react"
-
-export const TestComponent = () => {
-  const [toggle, setToggle] = useState(false)
-  return (
-    <button onClick={() => setToggle(!toggle)}>{toggle ? "On" : "Off"}</button>
-  )
-}
