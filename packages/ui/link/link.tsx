6bc5dd0ae749a12bf77c7595d49fc2abab49d59f
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2025-07-30 14:43:52 +1000

    Refine styling

--- packages/ui/link/link.tsx
+++ packages/ui/link/link.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/ui/utils"
 
 const linkVariants = cva(
-  "text-primary focus-visible:border-ring focus-visible:ring-ring/50 inline-flex items-center gap-0.5 font-medium whitespace-nowrap underline-offset-4 transition-all outline-none focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+  "text-primary focus-visible:border-ring focus-visible:ring-ring/50 inline-flex items-center gap-0.5 font-medium whitespace-nowrap underline-offset-4 outline-none focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
   {
     variants: {
       size: {
