a6ec3c0e9f67cf643503b3efb000f8d2e0c2ed01
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2025-09-01 20:17:55 +1000

    Refine db:clean script (#145)

--- packages/db/scripts/clean.ts
+++ packages/db/scripts/clean.ts
@@ -1,17 +1,25 @@
-import { getTableName, sql } from "drizzle-orm"
+import { getTableName, is, sql } from "drizzle-orm"
+import { SQLiteTable } from "drizzle-orm/sqlite-core"
 import * as schema from "../schema"
 import { getLocalOrRemoteDb } from "../utils"
 
 async function main() {
   const db = getLocalOrRemoteDb()
+
   console.log("🧹 Cleaning database...")
+  const tables = Object.fromEntries(
+    Object.entries(schema).filter((keyValue) => is(keyValue[1], SQLiteTable)),
+  )
+
+  await db.run(sql.raw(`PRAGMA foreign_keys = OFF`))
 
   await db.run(sql.raw(`drop table if exists __drizzle_migrations`))
-  Object.values(schema).forEach(async (table) => {
-    const tableName = getTableName(table)
+  Object.values(tables).forEach(async (table) => {
+    const tableName = getTableName(table as SQLiteTable)
     await db.run(sql.raw(`drop table if exists ${tableName}`))
   })
 
+  await db.run(sql.raw(`PRAGMA foreign_keys = ON`))
   console.log("✅ Cleaning complete!")
 }
 
