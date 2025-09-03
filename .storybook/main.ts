752026c44ac5048069038a78438ef3fd91637155
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2025-09-03 11:22:04 +1000

    Remove unused Storybook addons (#154)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,12 +2,7 @@ import type { StorybookConfig } from "@storybook/react-vite"
 
 const config: StorybookConfig = {
   stories: ["../{apps,packages}/**/*.stories.@(ts|tsx|mdx)"],
-  addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-themes",
-    "@storybook/addon-docs",
-  ],
+  addons: ["@storybook/addon-docs", "@storybook/addon-themes"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
