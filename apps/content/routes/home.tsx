093124b2ba8ba81010980c2a1f77a43f0bdd92d2
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2025-09-24 09:36:39 +0200

    Initial adjustments

--- apps/content/routes/home.tsx
+++ apps/content/routes/home.tsx
@@ -11,8 +11,13 @@ export default function Home({
 }: Route.ComponentProps<typeof loader>) {
   return (
     <main className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center justify-center gap-2 bg-slate-100 px-10 py-8 shadow-sm">
-        <h1 className="text-4xl font-bold">PageZERO</h1>
+      <div className="flex flex-col items-center justify-center gap-6">
+        <h1 className="text-7xl font-extrabold animate-in blur-in-2xl duration-1000">
+          PageZERO
+        </h1>
+        <h2 className="text-lg animate-in slide-in-from-bottom fade-in duration-1000 text-gray-600 tracking-wide">
+          Full-stack web application starter for Cloudflare
+        </h2>
         {greetings.map((greeting) => (
           <p key={greeting.id}>{greeting.greeting}</p>
         ))}
