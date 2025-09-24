eb587f4a9169bb12df632de269ae0f3b3055bced
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2025-09-24 17:12:53 +0200

    Improve start page design (#165)

--- apps/content/routes/home.tsx
+++ apps/content/routes/home.tsx
@@ -1,3 +1,4 @@
+import { CircleCheck, CircleOff } from "lucide-react"
 import { greetings } from "@/db/schema"
 import type { Route } from "@/types/route"
 
@@ -11,11 +12,20 @@ export default function Home({
 }: Route.ComponentProps<typeof loader>) {
   return (
     <main className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center justify-center gap-2 bg-slate-100 px-10 py-8 shadow-sm">
-        <h1 className="text-4xl font-bold">PageZERO</h1>
-        {greetings.map((greeting) => (
-          <p key={greeting.id}>{greeting.greeting}</p>
-        ))}
+      <div className="flex flex-col p-4">
+        <div className="flex flex-col items-center gap-8 animate-in blur-in-2xl duration-700">
+          <CircleOff className="size-36" />
+          <h1 className="text-6xl font-extrabold ">PageZERO</h1>
+        </div>
+
+        <div className="text-center bg-gray-100 rounded-lg p-4 max-w-md mt-20 animate-in slide-in-from-bottom-4 fade-in duration-1000">
+          {greetings.map((greeting) => (
+            <p key={greeting.id}>
+              <CircleCheck className="size-6 text-green-500 align-top inline-block mr-2" />
+              {greeting.greeting}
+            </p>
+          ))}
+        </div>
       </div>
     </main>
   )
