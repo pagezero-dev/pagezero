74201bee192da90a0e37985981652aaa8cf5dc52
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2025-12-17 18:12:03 +1100

    feat: add db:migrate:production and db:migrate:preview scripts (#224)

--- packages/db/utils.ts
+++ packages/db/utils.ts
@@ -2,9 +2,10 @@ import { config } from "@dotenvx/dotenvx"
 import Database from "better-sqlite3"
 import { drizzle as drizzleLocal } from "drizzle-orm/better-sqlite3"
 import glob from "fast-glob"
+import wranglerConfig from "../../wrangler.json"
 import { drizzle as drizzleRemote } from "./drivers/d1-http"
 
-export function getLocalSqliteDbUrl() {
+function getLocalSqliteDbUrl() {
   const dbUrls = glob.sync("./.wrangler/state/v3/d1/**/*.sqlite")
 
   if (dbUrls.length > 1) {
@@ -38,5 +39,49 @@ export function getLocalOrRemoteDb() {
   } else {
     console.log("🔗 Connecting to local db...")
     return drizzleLocal(new Database(getLocalSqliteDbUrl()), { logger: true })
+  }
+}
+
+function isValidCloudflareEnv(
+  value?: string,
+): value is keyof typeof wranglerConfig.env {
+  return !!value && value in wranglerConfig.env
+}
+
+function getDatabaseId(cloudflareEnv?: string) {
+  if (
+    isValidCloudflareEnv(cloudflareEnv) &&
+    ["production", "preview"].includes(cloudflareEnv)
+  ) {
+    return wranglerConfig.env[cloudflareEnv].d1_databases[0].database_id
+  }
+
+  return wranglerConfig.d1_databases[0].database_id
+}
+
+export function getDbCredentials() {
+  config({ path: ".env", quiet: true, ignore: ["MISSING_ENV_FILE"] })
+
+  const isRemote =
+    isValidCloudflareEnv(process.env.CLOUDFLARE_ENV) &&
+    ["production", "preview"].includes(process.env.CLOUDFLARE_ENV)
+
+  const databaseId = getDatabaseId(process.env.CLOUDFLARE_ENV)
+
+  if (isRemote) {
+    return {
+      driver: "d1-http",
+      dbCredentials: {
+        accountId: process.env.CLOUDFLARE_ACCOUNT_ID,
+        token: process.env.CLOUDFLARE_API_TOKEN,
+        databaseId,
+      },
+    }
+  }
+
+  return {
+    dbCredentials: {
+      url: getLocalSqliteDbUrl(),
+    },
   }
 }
